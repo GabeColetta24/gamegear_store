348606f1fe8b69772357a415fb60494f5904faea
GabeColetta24 <houfmtub@students.codeinstitute.net>
2026-05-15 16:19:33 +0100

    Display products on homepage

--- products/views.py
+++ products/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 
 # Homepage view
-# Renders the main homepage template
+# Displays all active products on the homepage
 def home(request):
-    return render(request, 'products/home.html')
+
+    # Get all active products from database
+    products = Product.objects.filter(is_active=True)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/home.html', context)
