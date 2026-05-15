ea5f36ad7492d2de8f9650af8855bbd76909bfde
GabeColetta24 <houfmtub@students.codeinstitute.net>
2026-05-15 15:36:40 +0100

    Create product model and admin functionality

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+# Register Product model so products can be added and edited in admin
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# Product model
+# Stores each item available to buy in the shop
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    # Shows the product name in the Django admin panel
+    def __str__(self):
+        return self.name
